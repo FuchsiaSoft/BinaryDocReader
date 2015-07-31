8234ee7199bf2f10a0214a068def8ab237bc135d
FuchsiaSoft <dupoint17@gmail.com>
2015-07-31 10:26:26 +0100

    more Fibby goodness

--- BinaryDocReader/FileReading/Internals/FibElements/FibRgFcLcb97.cs
+++ BinaryDocReader/FileReading/Internals/FibElements/FibRgFcLcb97.cs
@@ -239,8 +239,164 @@ namespace FuchsiaSoft.BinaryWordDocReader.FileReading.Internals.FibElements
         /// </summary>
         public UInt32 lcbPlcfFldHdr { get; private set; }
 
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the Table Stream. 
+        /// A PlcFld begins at this offset and specifies the locations of 
+        /// field characters in the Footnote Document. All CPs in this PlcFld 
+        /// are relative to the starting position of the Footnote Document. 
+        /// All CPs in this PlcFld MUST be greater than or equal to zero and 
+        /// less than or equal to FibRgLw97.ccpFtn. If lcbPlcfFldFtn is zero, 
+        /// fcPlcfFldFtn is undefined, and MUST be ignored.
+        /// </summary>
+        public UInt32 fcPlcfFldFtn { get; private set; }
 
+        /// <summary>
+        /// An unsigned integer that specifies the size, in bytes, 
+        /// of the PlcFld at offset fcPlcfFldFtn in the Table Stream.
+        /// </summary>
+        public UInt32 lcbPlcfFldFtn { get; private set; }
 
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the Table Stream. 
+        /// A PlcFld begins at this offset and specifies the locations of 
+        /// field characters in the Comment Document. All CPs in this 
+        /// PlcFld are relative to the starting position of the Comment 
+        /// Document. All CPs in this PlcFld MUST be greater than or 
+        /// equal to zero and less than or equal to FibRgLw97.ccpAtn. 
+        /// If lcbPlcfFldAtn is zero, fcPlcfFldAtn is undefined and MUST
+        /// be ignored.
+        /// </summary>
+        public UInt32 fcPlcfFldAtn { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, in bytes, of 
+        /// the PlcFld at offset fcPlcfFldAtn in the Table Stream.
+        /// </summary>
+        public UInt32 lcbPlcfFldAtn { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the Table 
+        /// Stream. An SttbfBkmk that contains the names of the 
+        /// bookmarks (1) in the document begins at this offset. If 
+        /// lcbSttbfBkmk is zero, fcSttbfBkmk is undefined and MUST 
+        /// be ignored.
+        /// This SttbfBkmk is parallel to the PlcfBkf at offset 
+        /// fcPlcfBkf in the Table Stream. Each string specifies 
+        /// the name of the bookmark (1) that is associated with 
+        /// the data element which is located at the same offset 
+        /// in that PlcfBkf. For this reason, the SttbfBkmk that 
+        /// begins at offset fcSttbfBkmk, and the PlcfBkf that 
+        /// begins at offset fcPlcfBkf, MUST contain the same number 
+        /// of elements.
+        /// </summary>
+        public UInt32 fcSttbfBkmk { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, in bytes, 
+        /// of the SttbfBkmk at offset fcSttbfBkmk.
+        /// </summary>
+        public UInt32 lcbSttbfBkmk { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the 
+        /// Table Stream. A PlcfBkf that contains information about 
+        /// the standard bookmarks (1) in the document begins 
+        /// at this offset. If lcbPlcfBkf is zero, fcPlcfBkf is 
+        /// undefined and MUST be ignored.
+        /// Each data element in the PlcfBkf is associated, in 
+        /// a one-to-one correlation, with a data element in the 
+        /// PlcfBkl at offset fcPlcfBkl. For this reason, the PlcfBkf 
+        /// that begins at offset fcPlcfBkf, and the PlcfBkl that 
+        /// begins at offset fcPlcfBkl, MUST contain the same number 
+        /// of data elements. This PlcfBkf is parallel to the SttbfBkmk 
+        /// at offset fcSttbfBkmk in the Table Stream. Each data element 
+        /// in the PlcfBkf specifies information about the 
+        /// bookmark (1) that is associated with the element which is 
+        /// located at the same offset in that SttbfBkmk. For this 
+        /// reason, the PlcfBkf that begins at offset fcPlcfBkf, 
+        /// and the SttbfBkmk that begins at offset fcSttbfBkmk, 
+        /// MUST contain the same number of elements.
+        /// The largest value that a CP marking the start or end 
+        /// of a standard bookmark (1) is allowed to have is the 
+        /// CP representing the end of all document parts.
+        /// </summary>
+        public UInt32 fcPlcfBkf { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, in bytes, 
+        /// of the PlcfBkf at offset fcPlcfBkf.
+        /// </summary>
+        public UInt32 lcbPlcfBkf { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the 
+        /// Table Stream. A PlcfBkl that contains information 
+        /// about the standard bookmarks (1) in the document begins
+        /// at this offset. If lcbPlcfBkl is zero, fcPlcfBkl 
+        /// is undefined and MUST be ignored.
+        /// Each data element in the PlcfBkl is associated, 
+        /// in a one-to-one correlation, with a data element 
+        /// in the PlcfBkf at offset fcPlcfBkf. For this reason, 
+        /// the PlcfBkl that begins at offset fcPlcfBkl, and the 
+        /// PlcfBkf that begins at offset fcPlcfBkf, MUST contain 
+        /// the same number of data elements.
+        /// The largest value that a CP marking the start or end 
+        /// of a standard bookmark (1) is allowed to have is the 
+        /// value of the CP representing the end of all document 
+        /// parts.
+        /// </summary>
+        public UInt32 fcPlcfBkl { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, 
+        /// in bytes, of the PlcfBkl at offset fcPlcfBkl.
+        /// </summary>
+        public UInt32 lcbPlcfBkl { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the offset 
+        /// in the Table Stream of a Tcg that specifies 
+        /// command-related customizations. If lcbCmds is 
+        /// zero, fcCmds is undefined and MUST be ignored.
+        /// </summary>
+        public UInt32 fcCmds { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, 
+        /// in bytes, of the Tcg at offset fcCmds.
+        /// </summary>
+        public UInt32 lcbCmds { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies an offset 
+        /// in the Table Stream. The PrDrvr, which contains 
+        /// printer driver information (the names of drivers, 
+        /// port, and so on), begins at this offset. If 
+        /// lcbPrDrvr is zero, fcPrDrvr is undefined and 
+        /// MUST be ignored.
+        /// </summary>
+        public UInt32 fcPrDrvr { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, 
+        /// in bytes, of the PrDrvr at offset fcPrDrvr.
+        /// </summary>
+        public UInt32 lcbPrDrvr { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies an offset in the 
+        /// Table Stream. The PrEnvPort that is the print 
+        /// environment in portrait mode begins at this offset. 
+        /// If lcbPrEnvPort is zero, fcPrEnvPort is undefined 
+        /// and MUST be ignored.
+        /// </summary>
+        public UInt32 fcPrEnvPort { get; private set; }
+
+        /// <summary>
+        /// An unsigned integer that specifies the size, in bytes, 
+        /// of the PrEnvPort at offset fcPrEnvPort.
+        /// </summary>
+        public UInt32 lcbPrEnvPort { get; private set; }
 
         protected void ReadFibRgFcLcb97(byte[] data)
         {
